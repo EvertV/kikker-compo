c8edf03e201e594e96b9d26a174e288493e7c526
Evert Vanderstadt <evert.vanderstadt.w9991@student.hogent.be>
2018-12-26 10:42:55 +0100

    delete scoreboard and move code to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
 import "./App.css";
-import Scoreboard from "./components/scoreboard";
+import TableCompetitors from "./components/tableCompetitors";
+import ManageCompetitors from "./components/manageCompetitors";
 
 class App extends Component {
+  state = {
+    competitors: [
+      { name: "Mike", points: 50 },
+      { name: "Ken", points: 5 },
+      { name: "Sieger", points: 15 }
+    ]
+  };
   render() {
-    return <Scoreboard />;
+    return (
+      <React.Fragment>
+        <div className="page-header">
+          <h1>
+            Kikker Compo <small>Gateway Gaming</small>
+          </h1>
+        </div>
+        <ManageCompetitors
+          competitors={this.state.competitors}
+          onAddCompetitor={this.handleAddCompetitor}
+        />
+
+        <h3>Scorebord</h3>
+        <TableCompetitors competitors={this.state.competitors} />
+      </React.Fragment>
+    );
   }
+  handleAddCompetitor = name => {
+    if (
+      name.trim() !== "" &&
+      !this.state.competitors.some(
+        c => c["name"].toLowerCase().trim() === name.toLowerCase().trim()
+      )
+    ) {
+      this.setState(prevState => ({
+        competitors: [
+          ...prevState.competitors,
+          {
+            name: name,
+            points: 0
+          }
+        ]
+      }));
+    }
+  };
 }
 
 export default App;
